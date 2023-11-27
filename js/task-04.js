29c2fca97f0b0e98083b0789dc3a47730e8f9dd9
TrotskyuYurii <tro2001@gmail.com>
2023-11-27 23:29:10 +0200

    task-4

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,23 @@
+const registerForm = document.querySelector(".login-form");
+
+registerForm.addEventListener("submit", event => {
+    event.preventDefault();
+
+    const inputElement = [registerForm.elements.email, registerForm.elements.password];
+    let dataObject = {};
+
+    inputElement.forEach(elementsInput=>{
+        if (elementsInput.value.trim()!==''){
+            dataObject[elementsInput.name]  =  elementsInput.value.trim(); 
+        } else {
+            alert('All form fields must be filled in');
+        }
+    })
+
+    registerForm.reset();
+
+    console.log(dataObject);
+
+});
+
 
