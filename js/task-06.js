c33db2d09679fab1c1c597329985acf1258563f0
TrotskyuYurii <tro2001@gmail.com>
2023-11-27 23:45:10 +0200

    task-6

--- js/task-06.js
+++ js/task-06.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  const inputElement = document.querySelector('#controls input');
+  const createButton = document.querySelector('[data-create]');
+  const destroyButton = document.querySelector('[data-destroy]');
+  const boxesContainer = document.querySelector('#boxes');
+
+  function createBoxes(amount) {
+    for (let i = 0; i < amount; i++) {
+      const box = document.createElement('div');
+      box.style.width = `${30 + i * 10}px`;
+      box.style.height = `${30 + i * 10}px`;
+      box.style.backgroundColor = getRandomHexColor();
+      boxesContainer.appendChild(box);
+    }
+  }
+
+  function destroyBoxes() {
+    boxesContainer.innerHTML = '';
+  }
+
+  createButton.addEventListener('click', function () {
+    const amount = parseInt(inputElement.value);
+    if (amount >= 1 && amount <= 100) {
+      createBoxes(amount);
+      inputElement.value = '';
+    } else {
+      alert('Please enter a number between 1 and 100.');
+    }
+  });
+
+  destroyButton.addEventListener('click', destroyBoxes);
+});
